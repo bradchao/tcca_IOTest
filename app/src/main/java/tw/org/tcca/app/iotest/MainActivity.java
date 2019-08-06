2cbfe70f63acf5b570181ccc57f53d5ade17faa2
Brad <brad@brad.tw>
2019-08-06 19:16:31 +0800

    1

---
+++ app/src/main/java/tw/org/tcca/app/iotest/MainActivity.java
@@ -0,0 +1,24 @@
+package tw.org.tcca.app.iotest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
+    }
+    public void test2(View view) {
+    }
+    public void test3(View view) {
+    }
+    public void test4(View view) {
+    }
+}
